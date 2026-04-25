4b94078454535b35c05fb90f453a4db1442c2c90
Jeremy Monteiro <jeremyjjm258@gmail.com>
2026-04-25 12:23:41 +0200

    Add files via upload

---
+++ OpenAI_Chatbot.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+user = OpenAI(api_key = "OPENAI-apikey")
+
+def chat_with_gpt(prompt):
+    response = user.chat.completions.create(
+        model = "gpt-4.1-mini",
+        messages = [{"role":"user","content": prompt}]
+    )
+
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit","exit","bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
